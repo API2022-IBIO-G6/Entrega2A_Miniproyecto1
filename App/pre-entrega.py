8b77275a2fd938d0ebadccced4471ac59677fe5a
maperezp <98606581+maperezp@users.noreply.github.com>
2022-02-16 11:48:03 -0500

    prueba funcion 1

--- App/pre-entrega.py
+++ App/pre-entrega.py
@@ -1,4 +1,4 @@
-import cv2
+import cv as cv2
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from skimage.morphology import label
@@ -8,19 +8,7 @@ import utils
 import sys 
 np.set_printoptions(threshold=sys.maxsize)
 
-
-matrix_1= np.zeros((50,50))
-matrix_1[2:10,2:10]=1
-matrix_1[25:30,25:30]=1
-
-matrix_2= np.zeros((50,50))
-matrix_2[2:10,2:10]=1
-matrix_2[25:30,25:30]=1
-
-matrix_3= np.zeros((50,50))
-matrix_3[2:10,2:10]=1
-matrix_3[25:30,25:30]=1
-def Mask2Detection(mask, img_id):
+def Mask2DetectionDibuja(mask, img_id):
 
     label_image = label(mask, connectivity=1)
     print(label_image)
@@ -64,15 +52,70 @@ def Mask2Detection(mask, img_id):
     print(predicciones)
     return predicciones
 
-    
-Mask2Detection(matrix_2, 1)
-"""
-split={}
+def Mask2Detection(mask, img_id):
+
+    label_image = label(mask, connectivity=1)
+    predicciones = []
+    print("# de anotaciones detectadas", len(regionprops(label_image)))
+    for region in regionprops(label_image):
+
+        min_row, min_col, max_row, max_col = region.bbox
+        
+        print("print:",min_row, min_col, max_row, max_col)
+        
+        #score = utils.pred_score(mask)
+        #print(score)
+        
+        dic = {"image_id": img_id, 
+            "category_id": 1,
+            "bbox": [
+                min_row,
+                min_col,
+                max_row-min_row,
+                max_col -min_col
+            ],
+            "score": 1}
+        predicciones.append(dic)
+    print(predicciones)
+    return predicciones
+
+#PRUEBA
+matrix_1= np.zeros((50,50))
+matrix_1[2:10,2:10]=1
+matrix_1[25:30,25:30]=1
+
+matrix_2= np.zeros((50,50))
+matrix_2[2:10,2:10]=1
+matrix_2[25:30,25:30]=1
+
+matrix_3= np.zeros((50,50))
+matrix_3[2:10,2:10]=1
+matrix_3[25:30,25:30]=1
+
+prueba = [matrix_1, matrix_2, matrix_3]
 predicciones = []
 img_id= 1
 
-for img in split:
-    mask = split[img]
-    predicciones.append(Mask2Detection(mask, img_id))
+plt.figure("ImagenesBinarias.png")
+fig, ax = plt.subplots(figsize=(10, 6))
+
+for img in prueba:
+    mask = prueba[img]
+
+    prediccion = Mask2Detection(mask, img_id)
+
+    # Obtenemos vector del bbox
+    min_row, min_col, max_row, max_col, = prediccion["bbox"]
+
+    # draw rectangle around segmented coins
+    rect = mpatches.Rectangle((min_col-1, min_row-1), max_col - min_col+1, max_row - min_row+1,fill=False, edgecolor='red', linewidth=1)
+    
+    ax.add_patch(rect)
+
+    plt.subplot(3,2,img_id)
     img_id +=1
-"""
+
+ax.set_axis_off()
+plt.tight_layout()
+plt.show()
+    
