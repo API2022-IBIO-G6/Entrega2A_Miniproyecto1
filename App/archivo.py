01eeac542fe9e3227f4c3522a90df7110581ca57
Lina-go <77931393+Lina-go@users.noreply.github.com>
2022-02-18 16:49:19 -0500

    commit
    ksdnckrs,cnelc

--- App/archivo.py
+++ App/archivo.py
@@ -4,49 +4,46 @@ import matplotlib.patches as mpatches
 from skimage.morphology import label
 from skimage.measure import regionprops
 import numpy as np
-import config as cf
-assert cf
-import data_mp1.utils as ut
-import sys 
 import json
 import os
-import matplotlib.pyplot as plt
-import numpy as np
 from scipy.integrate import simps
 
 # 8.2. Función predicciones de detección 
 
-from ast import Param
-
-
-def Mask2Detection_Codigo1_Codigo2(mask, img_id):
+def Mask2Detection_201923972_201923531(mask, img_id):
     """
     @param mask: Mascara binaria de la cual se extraerán las predicciones de anotación.
     @param img_id: Id de la imagen
     @return: Lista de diccionarios con las predicciones. Esta debe tener el mismo formato
     ejemplificado en el archivo “dummy_predictions.json”.
     """
-    label_image = label(mask, connectivity=1)
     predicciones = []
+    # Obtenemos una matriz etiquetada donde a todas las regiones conectadas se les asigna el mismo valor entero
+    label_image = label(mask, connectivity=1) 
     print("# de anotaciones detectadas", len(regionprops(label_image)))
+
+    # Recorremos cada una de las regiones conectadas 
     for region in regionprops(label_image):
 
+        #Obtenemos las coordenadas del rectangulo dilimitador de la región []
         min_row, min_col, max_row, max_col = region.bbox
         
-        print("print:",min_row, min_col, max_row, max_col)
+        print("El rectangulo delimitador está en el intervalo [{}, {}), [{},{})".format(min_row, max_row, min_col, max_col))
         
+        # Creamos el diccionario asociado a la predicción a una prediccion
         dic = {"image_id": img_id, 
-            "category_id": 1,
+            "category_id": 3,
             "bbox": [
                 min_row,
                 min_col,
-                max_row-min_row,
-                max_col -min_col
+                max_row-min_row, # ancho
+                max_col-min_col  # alto
             ],
-            "score": 1}
-        predicciones.append(dic)
+            "score": 1 #valor arbitrario 
+            } 
+        predicciones.append(dic) 
     
-    return predicciones
+    return predicciones 
 
 # 8.2.1 Validación de la función de predicción de detección    
 """
@@ -56,6 +53,7 @@ las 3 imágenes binarias diseñadas, elaboren un subplot de 3x2 donde se pueda v
 la máscara de segmentación original en la primera columna y la anotación de detección 
 correspondiente en la otra columna.
 """
+print( "8.2.1 Validación de la función de predicción de detección " )
 # Inicializamos tres matrices 50x50 de ceros 
 matrix_1, matrix_2, matrix_3= np.zeros((50,50)), np.zeros((50,50)), np.zeros((50,50))
 
@@ -69,27 +67,27 @@ matrix_1[17:33,9:33], matrix_1[[16,33],11:31], matrix_1[[15,34],13:29] =1,1,1
 matrix_2[[19,20,39,40], 22:38], matrix_2[[18,41], 24:36], matrix_2[[21,22,37,38], 21:39], matrix_2[23:37, 20:40] = 1,1,1,1
 
 # Imagen Binaria 3
-matrix_3[21:30, [14,15,34,35]], matrix_3[22:29, [13,36]], matrix_3[20:31, 16:34] = 1, 1, 1  # ES UN CUADRADO !!!!!!!!
+matrix_3[21:30, [14,15,34,35]], matrix_3[22:29, [13,36]], matrix_3[20:31, 16:34] = 1, 1, 1  
 
 masks = [matrix_1, matrix_2, matrix_3]
 lis_json = []
-i= 0
+i= 0 # variable para el img_id
 fig, ax = plt.subplots(nrows=3,ncols=2)
 for mask in masks:
+    # Se grafica la amscara de segmentación original
     ax[i,0].imshow(mask)
     ax[i,0].set_axis_off()
     ax[i,1].imshow(mask)
     ax[i,1].set_axis_off()
-    predicciones = Mask2Detection_Codigo1_Codigo2(mask, i)
-    
+    # se obtiene la prediccion asociada
+    predicciones = Mask2Detection_201923972_201923531(mask, i)    
     for p in predicciones:
-        print(p)
+        # Se grafica la anotación correspondiente
         rect = mpatches.Rectangle(((p["bbox"][1])-1.1, (p["bbox"][0])-1.1), (p["bbox"][3])+1.1, (p["bbox"][2])+1.1,
                                 fill=False, edgecolor='red', linewidth=2)
         ax[i,1].add_patch(rect)
     i += 1
     lis_json += predicciones
-    print("----------------\n",lis_json)
 plt.suptitle("Validación de la función de predicción de detección")
 plt.tight_layout()
 plt.show()
@@ -101,41 +99,35 @@ predicciones = os.path.join("data_mp1","dummy_predictions.json")
 
 def get_iou(a, b, epsilon=1e-5):
     """ 
-    Given two boxes `a` and `b` defined as a list of four numbers:
+    Dados dos bbox `a` y `b` definidas cada una como una lista de cuatro valores de coordenadas:
             [x1,y1,x2,y2]
-        where:
-            x1,y1 represent the upper left corner
-            x2,y2 represent the lower right corner // tiene que representar w h 
-        It returns the Intersect of Union score for these two boxes.
-    Args: 
-        a:          (list of 4 numbers) [x1,y1,x2,y2] // [x,y,w,h]
-        b:          (list of 4 numbers) [x1,y1,x2,y2] // [x,y,w,h]
-        epsilon:    (float) Small value to prevent division by zero
-    Returns:
-        (float) The Intersect of Union score.
+        donde:
+            x1,y1 representan las coordenadas de la esquina superior izquierda
+            x2,y2 representan el ancho (w) y el alto (h) 
+        epsilon:    (float) pequeño valor para prevenir división por cero
+    Retorno:
+        (float)  retorna el score para la intercecion de la union para las dos bbox 
     """
-    # COORDINATES OF THE INTERSECTION BOX
+    # COORDENADAS DEL RECTANGULO DE INTERCECION
     x1 = max(a[0], b[0])
     y1 = max(a[1], b[1])
     x2 = min(a[0]+a[2], b[0]+b[2])
     y2 = min(a[1]+a[3], b[1]+b[3])
-    #x2 = min(a[2], b[2])
-    #y2 = min(a[3], b[3])
 
-    # AREA OF OVERLAP - Area where the boxes intersect
+    # AREA DE SOBRELAPAMIENTO- Area donde las dos box se intersectan
     width = (x2 - x1)
     height = (y2 - y1)
-    # handle case where there is NO overlap
+    # Si no hay sobrelapamiento retornamos 0
     if (width<0) or (height <0):
         return 0.0
     area_overlap = width * height
 
-    # COMBINED AREA
+    # AREA DE LA UNION
     area_a = (a[2]) * (a[3])
     area_b = (b[2]) * (b[3])
     area_combined = area_a + area_b - area_overlap
 
-    # RATIO OF AREA OF OVERLAP OVER COMBINED AREA
+    # Proporción Area de la intersecion / Area de la unión
     iou = area_overlap / (area_combined+epsilon)
     return iou
 
@@ -151,37 +143,32 @@ def detections_Codigo1_Codigo2(conf_thresh=0.5, jaccard_thresh=0.7, annot_file=c
     falsos negativos y TN: Cantidad de verdaderos negativos.
     """
     
-    lista=[]
-    lista_aux =[]
     with open(annot_file) as f:
         annotations = json.load(f)
     with open(pred_file) as f:
         predictions = json.load(f)
     
     TP, FP, FN = 0, 0, 0
-    #Intersection Over Union (IOU)
-    
     for pred in predictions:    
         pred_bbox, pred_category, pred_image_id= pred["bbox"], pred["category_id"], pred["image_id"]
         for annot in annotations["annotations"]:
             annot_category, annot_image_id= annot["category_id"], annot["image_id"]
             if annot_image_id == pred_image_id:
                 if pred_category == annot_category:
-                    lista_aux.append(0)
+                
                     annot_bbox = annot["bbox"]
+                    #Intersection Over Union (IOU) 
                     iou = get_iou(pred_bbox, annot_bbox)
+
                     if pred["score"] >= conf_thresh:
                         if iou >= jaccard_thresh:
                             TP += 1
-                            lista.append("TP")
                         elif iou <= jaccard_thresh:
-                            FP += 1 
-                            lista.append("FP")         
+                            FP += 1        
                     else:
-                        lista.append("FN")
                         FN += 1
     TN = 0                    
-    return TP,FP,FN, TN ## REVISAR FN!!!!!!!!!!
+    return TP,FP,FN, TN 
 
 # 8.3.1 Validación de la función de evaluación de predicciones
 """
@@ -191,11 +178,26 @@ Jaccard de 0.7, deben calcular las métricas especificadas en la función junto 
 precisión, cobertura y f-medida asociadas. 
 """
 TP,FP,FN, TN =detections_Codigo1_Codigo2()
-
-print("Confusion Matrix : Anotacion (A), Prediccion (P)")
+print("\n8.3.1 Validación de la función de evaluación de predicciones")
+print("\nConfusion Matrix : Anotacion (A), Prediccion (P)")
+matrix = np.array([[TP,FP],[FN,TN]])
 print("{:25}\t{:20}\t{:20}\t".format("", "(A) Celulas blancas","(A) NO Celulas blancas" ))
 print("{:15}\t{:20}\t{:20}\t".format("(P) Celulas blancas", TP, FP))
 print("{:15}\t{:20}\t{:20}\t".format("(P) NO Celulas blancas", FN, TN))
+
+print("\nConfusion Matrix Normalizada por filas: Anotacion (A), Prediccion (P)")
+row_sums = matrix.sum(axis=1)
+normalize_row = np.round(matrix / row_sums[:, np.newaxis], 2)
+print("{:25}\t{:20}\t{:20}\t".format("", "(A) Celulas blancas","(A) NO Celulas blancas" ))
+print("{:15}\t{:20}\t{:20}\t".format("(P) Celulas blancas", normalize_row[0, 0], normalize_row[0, 1]))
+print("{:15}\t{:20}\t{:20}\t".format("(P) NO Celulas blancas", normalize_row[1, 0], normalize_row[1, 1]))
+
+print("\nConfusion Matrix Normalizada por columnas: Anotacion (A), Prediccion (P)")
+col_sums = matrix.sum(axis=0)
+normalize_col = np.round(matrix / col_sums, 2)
+print("{:25}\t{:20}\t{:20}\t".format("", "(A) Celulas blancas","(A) NO Celulas blancas" ))
+print("{:15}\t{:20}\t{:20}\t".format("(P) Celulas blancas", normalize_col[0, 0], normalize_col[0, 1]))
+print("{:15}\t{:20}\t{:20}\t".format("(P) NO Celulas blancas", normalize_col[1, 0], normalize_col[1, 1]))
 
 #Metricas
 precision = round(TP/(TP+FP),2)
